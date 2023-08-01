b03fb3a437021cfb8ca1d13ac61566a45d60b5d7
dave caruso <me@paperdave.net>
2023-07-31 21:44:43 -0700

    its not perfect but almost done

--- src/linker.zig
+++ src/linker.zig
@@ -281,12 +281,6 @@ pub const Linker = struct {
                             }
                         }
 
-                        if (JSC.DisabledModule.has(import_record.path.text)) {
-                            import_record.path.is_disabled = true;
-                            import_record.do_commonjs_transform_in_printer = true;
-                            continue;
-                        }
-
                         // if (strings.eqlComptime(import_record.path.text, "process")) {
                         //     import_record.path.text = "node:process";
                         //     externals.append(record_index) catch unreachable;
